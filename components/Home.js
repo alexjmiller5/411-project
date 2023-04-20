2e77d6410bf2388c4387509365aba7fd3be0e3dc
hrothman <hrothman@bu.edu>
2023-04-20 17:38:27 -0400

    Oauth and Map
    I have Oauth working, in order to fully sign in the email must be added as a test user on the google cloud console. Additionally a basic example map is rendering within the WaterMap.js file.

--- components/Home.js
+++ components/Home.js
@@ -2,11 +2,11 @@ import { StyleSheet, View, Text } from 'react-native';
 
 import Button from './Button';
 
-export default function Home({user}) {
+export default function Home({user, navigation}) {
   return (
     <View style={styles.footerContainer}>
       <Text>Home Screen {user} </Text>
-      <Button label="Find Nearby Fountains" />
+      <Button label="Find Nearby Fountains" onPress={() => navigation.navigate('WaterMap')} />
       <Button label="Add Nearby Fountains" />
       <Button label="Show Eco Stats" />
     </View>
