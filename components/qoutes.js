f3b628d9ea36571ddf4d46358543b2becd723d19
hrothman <66038347+hrothman@users.noreply.github.com>
2023-05-04 01:04:25 -0400

    Changed Return Variable and made async

--- components/qoutes.js
+++ components/qoutes.js
@@ -1,23 +1,28 @@
-export function fetchApiCall() {
-
+export async function getRandomQuote(fail) {
+  if (fail > 10) {
+    return null
+  }
     var quote = "";
     var author = "";
-    fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=en", {
+    var resp = null;
+    await fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=en", {
       "method": "GET",
       "headers": {
         "x-rapidapi-host": "quotes15.p.rapidapi.com",
         "x-rapidapi-key": "b800edfc20msh96a2651d02c9a70p1bd011jsne85f644eae5f"
       }
     })
-      .then(response => response.json())
-      .then(response => {
-        console.log("content: ", response.content);
-        quote = response.content
-        console.log("name: ",response.originator.name)
-        author = response.originator.name
-      })
-      .catch(err => {
-        console.log("error: ",err);
-      });
-      return quote,author
+    .then(response => response.json())
+    .then(response => {
+      console.log("content: ", response.content);
+      quote = response.content
+      console.log("name: ",response.originator.name)
+      author = response.originator.name
+      resp= response
+    })
+    .catch(err => {
+      console.log("error: ",err);
+      //getRandomQuote(fail+1)
+    });
+    return resp
   }
